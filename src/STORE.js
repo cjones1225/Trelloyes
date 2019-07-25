959919ebf696f0c4036260ee2e7d48d0a30d9843
Colton Jones <coltoncejones12@gmail.com>
2019-07-25 13:19:57 -0400

    initial commit

---
+++ src/STORE.js
@@ -0,0 +1,39 @@
+export default {
+  lists: [
+    {
+      id: "1",
+      header: "First list",
+      cardIds: ["a", "b", "e", "f", "g", "j", "l", "m"]
+    },
+    {
+      id: "2",
+      header: "Second list",
+      cardIds: ["b", "c", "d", "f", "h", "i", "k"]
+    },
+    {
+      id: "3",
+      header: "Third list",
+      cardIds: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+    },
+    {
+      id: "4",
+      header: "Fourth list",
+      cardIds: ["l", "m"]
+    }
+  ],
+  allCards: {
+    a: { id: "a", title: "First card", content: "First Card" },
+    b: { id: "b", title: "Second card", content: "Second Card" },
+    c: { id: "c", title: "Third card", content: "Third Card" },
+    d: { id: "d", title: "Fourth card", content: "Fourth Card" },
+    e: { id: "e", title: "Fifth card", content: "Fifth Card" },
+    f: { id: "f", title: "Sixth card", content: "Sixth Card" },
+    g: { id: "g", title: "Seventh card", content: "Seventh Card" },
+    h: { id: "h", title: "Eighth card", content: "Eighth Card" },
+    i: { id: "i", title: "Ninth card", content: "Ninth Card" },
+    j: { id: "j", title: "Tenth card", content: "Tenth Card" },
+    k: { id: "k", title: "Eleventh card", content: "Eleventh Card" },
+    l: { id: "l", title: "Twelfth card", content: "Twelfth Card" },
+    m: { id: "m", title: "Thirteenth card", content: "Thirteenth Card" }
+  }
+};
